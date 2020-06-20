ed4bf420f973e6c8f7e5b5174a9014fffe71ca30
TeuTeuf <antoine.teuf@gmail.com>
2020-06-20 22:14:02 +0200

    shuffle deck on creation and not on draw

--- Assets/Scripts/Game/Deck.cs
+++ Assets/Scripts/Game/Deck.cs
@@ -12,6 +12,8 @@ namespace Game
     {
         [SerializeField] private Card prefabCard = default;
 
+        [SerializeField] private int nbCardsDuplicate = 2;
+
         private List<CardValue> _remainingCards;
 
         private void Awake()
@@ -27,9 +29,8 @@ namespace Game
                 return null;
             }
 
-            var cardIndex = Random.Range(0, _remainingCards.Count);
-            var cardValue = _remainingCards[cardIndex];
-            _remainingCards.RemoveAt(cardIndex);
+            var cardValue = _remainingCards[0];
+            _remainingCards.RemoveAt(0);
 
             var deckTransform = transform;
             var card = Instantiate(prefabCard, deckTransform.position, deckTransform.rotation);
@@ -40,14 +41,25 @@ namespace Game
 
         private void ResetRemainingCards()
         {
-            _remainingCards = new List<CardValue>();
+            var allCards = new List<CardValue>();
+            
             foreach (var suite in Enum.GetValues(typeof(CardSuite)).Cast<CardSuite>())
             {
                 foreach (var rank in Enum.GetValues(typeof(CardRank)).Cast<CardRank>())
                 {
-                    _remainingCards.Add(new CardValue {Rank = rank, Suite = suite});
-                    _remainingCards.Add(new CardValue {Rank = rank, Suite = suite});
+                    for (var i = 0; i < nbCardsDuplicate; i++)
+                    {
+                        allCards.Add(new CardValue {Rank = rank, Suite = suite});
+                    }
                 }
+            }
+
+            _remainingCards = new List<CardValue>();
+            while (allCards.Count > 0)
+            {
+                var cardIndex = Random.Range(0, allCards.Count);
+                _remainingCards.Add(allCards[cardIndex]);
+                allCards.RemoveAt(cardIndex);
             }
         }
     }
