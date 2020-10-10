9d7f2bf6ec4907699504ed774ab7eef440d9e7a7
TeuTeuf <antoine.teuf@gmail.com>
2020-10-10 12:19:04 +0200

    cards move

--- Assets/Scripts/Game/CardComponents/Card.cs
+++ Assets/Scripts/Game/CardComponents/Card.cs
@@ -1,15 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Game.CardComponents
 {
     public class Card : MonoBehaviour
     {
-        private CardVisual _cardVisual;
+        [SerializeField] private float moveSpeed = 10.0f;
+        
         public CardValue Value { get; private set; }
+        
+        private CardVisual _cardVisual;
+        private Vector3? _targetPosition;
 
         private void Awake()
         {
             _cardVisual = GetComponent<CardVisual>();
+        }
+
+        private void Update()
+        {
+            if (_targetPosition != null)
+            {
+                var transformPosition = transform.position;
+                transform.position = Vector3.MoveTowards(
+                    transformPosition,
+                    (Vector3) _targetPosition,
+                    Time.deltaTime * moveSpeed
+                );
+
+                if (transform.position == _targetPosition)
+                {
+                    _targetPosition = null;
+                }
+            }
         }
 
         public void SetValue(CardValue value)
@@ -21,7 +44,7 @@ namespace Game.CardComponents
         public void Move(Vector3 position, Quaternion rotation)
         {
             var cardTransform = transform;
-            cardTransform.position = position;
+            _targetPosition = position;
             cardTransform.rotation = rotation;
         }
     }
