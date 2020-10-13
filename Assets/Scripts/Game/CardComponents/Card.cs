dbcd58bc1b51c0dfea4691ae3473180e71a78ba3
TeuTeuf <antoine.teuf@gmail.com>
2020-10-13 23:03:46 +0200

    extract a Rotator component

--- Assets/Scripts/Game/CardComponents/Card.cs
+++ Assets/Scripts/Game/CardComponents/Card.cs
@@ -6,34 +6,15 @@ namespace Game.CardComponents
     public class Card : MonoBehaviour
     {
         [SerializeField] private Mover mover = default;
-        
-        [SerializeField] private float rotationSpeed = 180.0f;
+        [SerializeField] private Rotator rotator = default;
         
         public CardValue Value { get; private set; }
         
         private CardVisual _cardVisual;
-        private Quaternion? _targetRotation;
 
         private void Awake()
         {
             _cardVisual = GetComponent<CardVisual>();
-        }
-
-        private void Update()
-        {
-            if (_targetRotation != null)
-            {
-                transform.rotation = Quaternion.RotateTowards(
-                    transform.rotation,
-                    (Quaternion) _targetRotation,
-                    Time.deltaTime * rotationSpeed
-                );
-
-                if (transform.rotation == _targetRotation)
-                {
-                    _targetRotation = null;
-                }
-            }
         }
 
         public void SetValue(CardValue value)
@@ -45,7 +26,7 @@ namespace Game.CardComponents
         public void Move(Vector3 position, Quaternion rotation)
         {
             mover.Move(position, false, null);
-            _targetRotation = rotation;
+            rotator.Rotate(rotation);
         }
     }
 }
---
+++ Assets/Scripts/Game/Rotator.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed = 720.0f;
+        
+        private Quaternion? _targetRotation;
+
+        private void Update()
+        {
+            if (_targetRotation != null)
+            {
+                transform.rotation = Quaternion.RotateTowards(
+                    transform.rotation,
+                    (Quaternion) _targetRotation,
+                    Time.deltaTime * rotationSpeed
+                );
+
+                if (transform.rotation == _targetRotation)
+                {
+                    _targetRotation = null;
+                }
+            }
+        }
+
+        public void Rotate(Quaternion targetRotation)
+        {
+            _targetRotation = targetRotation;
+        }
+    }
+}
