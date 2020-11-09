d9b0175c0c4dfa55638821f609193381da23ad42
TeuTeuf <antoine.teuf@gmail.com>
2020-11-09 22:18:40 +0100

    add analytics

---
+++ Assets/Scripts/Menu/EventTracking.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEditor.Analytics;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+namespace Menu
+{
+    [RequireComponent(typeof(GameSave))]
+    public class EventTracking : MonoBehaviour
+    {
+        private GameSave _gameSave;
+
+        private void Awake()
+        {
+            _gameSave = GetComponent<GameSave>();
+            AnalyticsSettings.testMode = true;
+        }
+
+        public void TrackChangeScene(AvailableScene scene)
+        {
+            Analytics.CustomEvent("CHANGE_SCENE", new Dictionary<string, object>
+            {
+                {"scene", scene.ToString()},
+                {"playerId", _gameSave.LoadString(GameSave.SaveKey.PlayerId)},
+                {"playerPseudo", _gameSave.LoadString(GameSave.SaveKey.PlayerPseudo)},
+            });
+        }
+
+        public void TrackStartRun()
+        {
+            Analytics.CustomEvent("START_RUN", new Dictionary<string, object>
+            {
+                {"playerId", _gameSave.LoadString(GameSave.SaveKey.PlayerId)},
+                {"playerPseudo", _gameSave.LoadString(GameSave.SaveKey.PlayerPseudo)},
+                {"selectedRule", _gameSave.LoadString(GameSave.SaveKey.SelectedRule)},
+            });
+        }
+
+        public void TrackFail()
+        {
+            Analytics.CustomEvent("FAIL", GetGameInfos());
+        }
+
+        public void TrackSuccess()
+        {
+            Analytics.CustomEvent("SUCCESS", GetGameInfos());
+        }
+
+        public void TrackValidation()
+        {
+            Analytics.CustomEvent("VALIDATION", GetGameInfos());
+        }
+
+        private Dictionary<string, object> GetGameInfos()
+        {
+            return new Dictionary<string, object>
+            {
+                {"playerId", _gameSave.LoadString(GameSave.SaveKey.PlayerId)},
+                {"playerPseudo", _gameSave.LoadString(GameSave.SaveKey.PlayerPseudo)},
+                {"selectedRule", _gameSave.LoadString(GameSave.SaveKey.SelectedRule)},
+                {"lastNbActions", _gameSave.LoadString(GameSave.SaveKey.LastNbActions)},
+                {"lastNbErrors", _gameSave.LoadString(GameSave.SaveKey.LastNbErrors)},
+            };
+        }
+    }
+}
--- Assets/Scripts/Menu/SceneSwitcher.cs
+++ Assets/Scripts/Menu/SceneSwitcher.cs
@@ -15,8 +15,16 @@ namespace Menu
         Validate
     }
 
+    [RequireComponent(typeof(EventTracking))]
     public class SceneSwitcher : MonoBehaviour
     {
+        private EventTracking _eventTracking;
+
+        private void Awake()
+        {
+            _eventTracking = GetComponent<EventTracking>();
+        }
+
         public void SwitchToHome() => Switch(AvailableScene.Home);
         public void SwitchToSelectRule() => Switch(AvailableScene.SelectRule);
         public void SwitchToGame() => Switch(AvailableScene.Game);
@@ -27,6 +35,28 @@ namespace Menu
 
         public void Switch(AvailableScene scene)
         {
+            _eventTracking.TrackChangeScene(scene);
+
+            if (scene == AvailableScene.Game)
+            {
+                _eventTracking.TrackStartRun();
+            }
+
+            if (scene == AvailableScene.Fail)
+            {
+                _eventTracking.TrackFail();
+            }
+
+            if (scene == AvailableScene.Success)
+            {
+                _eventTracking.TrackSuccess();
+            }
+
+            if (scene == AvailableScene.Validate)
+            {
+                _eventTracking.TrackValidation();
+            }
+            
             switch (scene)
             {
                 case AvailableScene.Home:
